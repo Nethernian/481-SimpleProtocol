2cb7324d8a49c5a6d5abb2a0a8f1f213b5c3f1d6
Nathan Wooddell <Nathanwooddell@gmail.com>
2023-10-09 16:21:55 -0400

    Setup

---
+++ Proj1Lib.py
@@ -0,0 +1,10 @@
+# CMSC 481 Project 1 -- Proj1Lib.py
+# Nathan Woodell & Sean Arfa Russo
+
+# This file contains the majority of the code for CMSC 481's project 1.
+# For this project we must develop a Application Layer web protocol, which we
+# will simulate using the UMBC cyberrange, and the python sockets package. The
+# requirements for the project are outlined in the file "ProjectDocument.pdf".
+
+# Import Statements
+import socket
---
+++ driver.py
@@ -0,0 +1,10 @@
+# CMSC 481 Project 1 -- driver.py
+# Nathan Woodell & Sean Arfa Russo
+
+# This project will serve as a basic driver file for CMSC 481's Project 1.
+# We have been tasked with creating a Simple Application Protocol - the outline
+# for this project is contained in the file titled "ProjectDocument.pdf". The
+# code used by this driver is contained in the file "Proj1Lib.py"
+
+# Import Statements
+import Proj1Lib
