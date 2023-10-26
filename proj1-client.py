52b0d2fa7ecab749f163715193c7065cfdbc032a
NathanWooddell2 <41447340+Nethernian@users.noreply.github.com>
2023-10-26 12:03:00 -0400

    refactor
    renamed a bunch of documents, we now have a library, a server program implementing it, and a client program, which also implements our library.

---
+++ proj1-client.py
@@ -0,0 +1,9 @@
+# CMSC 481 Project 1 -- proj1-client.py
+# Nathan Woodell & Sean Arfa Russo
+
+# This file contains the client program for our custom network protocol.
+# This program requires the file Proj1Lib.py to function correctly.
+# For this project it is necessary to run 
+
+# Import Statements
+import Proj1Lib
